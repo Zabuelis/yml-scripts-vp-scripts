48e26d0863563f1bce56c216453e129b4379f0d1
Zabuelis <karoliszabulis@gmail.com>
2024-10-02 19:05:19 +0300

    minor edit

--- build_nginx/nginx_build.sh
+++ build_nginx/nginx_build.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 
 func_install () {
-	mv default ~/
-	mv info.php ~/
 	apt update -y
 	apt install nginx -y
 	apt install php-fpm -y
-	cp ~/default /etc/nginx/sites-available/
+	mv ~/default /etc/nginx/sites-available/
 	systemctl restart nginx
 	mv info.php /var/www/html/
-	rm -f ~/default
-	rm -f ~/info.php
 }
 
 
-sudo func_install
+func_install
