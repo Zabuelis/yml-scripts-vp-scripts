291a41abbd6ad6a53a45116d7059834134213994
Karolis Zabulis <karoliszabulis@gmail.com>
2024-10-17 14:45:20 +0300

    docker update

--- build_docker/docker.sh
+++ build_docker/docker.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 #Ubuntu dependant
 sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update -y
 sudo apt install -y docker-ce
+sudo systemctl status docker
 
 
 sudo docker run hello-world
