5bb23a45aa32fe58cd5076fa3d122cc471fec76f
Karolis Zabulis <karoliszabulis@gmail.com>
2024-10-24 09:38:49 +0300

    Finishing up expression_replacer.sh
    Finished expression_replacer.sh
    Documentation implementation

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-For private usage only
+# Project ToiToiTracker Website
+### build_nginx
+---
+- Downloads and install webserver **nginx**
+- Overwrites the configuration and adds php modules
+- Places a file called **info.php** into **www** catalogue
+- **PORT 80 HAS TO BE OPEN**
+- Stores some of the necessary html files for the project
+
+### build_docker
+---
+- Downloads and installs docker
+- Downloads and install docker-compose
+
+### sample-project
+---
+- Stores concept files for docker image for the end project
+- Should be used as a reference not the end product
+
+### expression_replacer.sh
+- Requires 2 parameters **directory to edit** and **file that has all the patterns**
+- Patterns file should have formatation such as patternToSearch,patternToReplace
+- Pattern pairs should be in one line and separated by comma
+
+---
+**For private usage only**\
+**Html files rights reserved by [toitoi.lt](https://www.toitoi.lt/)**
--- expression_replacer.sh
+++ expression_replacer.sh
@@ -1,40 +1,37 @@
 #!/usr/bin/env bash
 
 command_exists() {
-    type "$1" &> /dev/null ;
-}
-
-escape_special_chars() {
-    local input="$1"
-    printf '%s\n' "$input" | sed -e 's/[\/&]/\\&/g' -e 's/[\|$^*+?.(){}[]]/\\&/g'
+        type "$1" &> /dev/null ;
 }
 
 
 if ! command_exists sed ; then
-    echo "The 'sed' command is required but not installed. Please install 'sed' and try again."
-    exit 1
+        echo "The 'sed' command is required but not installed. Please install 'sed' and try again."
+        exit 1
 fi
 
-if [ "$#" -ne 3 ] ; then
-        echo "Usage: $0 <directory> <search_expression> <replacement_expression>"
+if [ "$#" -ne 2 ]; then
+        echo "Usage: $0 <directory> <pattern_file>"
         exit 1
 fi
 
 dir_path="$1"
-search_expr="$2"
-replacement_expr="$3"
+pattern_file="$2"
 
 if [ ! -d "$dir_path" ]; then
-    echo "The directory '$dir_path' does not exist."
-    exit 1
+        echo "The directory '$dir_path' does not exist."
+        exit 1
 fi
 
-escaped_search_expr=$(escape_special_chars "$search_expr")
-#escaped_replace_expr=$(escape_special_chars "$replace_expr")
+if [ ! -f "$pattern_file" ]; then
+        echo "Pattern file does not exist"
+        exit 1
+fi
 
-echo "Escaped Search Expression: $escaped_search_expr"
-echo "Escaped Replacement Expression: $escaped_replace_expr"
-
-find "$dir_path" -type f -exec sed -i "s|$escaped_search_expr|$replacement_expr|g" {} +
+while IFS=, read -r search_expr replacement_expr; do
+        if [[ -n "search_expr" && -n "$replacement_expr" ]]; then
+                find "$dir_path" -type f -exec sed -i "s|$search_expr|$replacement_expr|g" {} +
+        fi
+done < "$pattern_file"
 
 echo "Replacement complete."
